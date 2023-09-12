6313b29ffc6d6d5422a5dcea9493f9f57b484a04
orw22 <oliver@thewortleys.plus.com>
2023-09-12 04:11:22 +0100

    img formats

--- src/consts.rs
+++ src/consts.rs
@@ -2,4 +2,4 @@ pub enum Constants {
     MaxFileSize = 2000000, // 2MB
 }
 
-pub const IMAGE_EXTENSIONS: [&str; 4] = ["jpg", "jpeg", "png", "bmp"];
+pub const IMAGE_EXTENSIONS: [&str; 7] = ["jpg", "jpeg", "png", "bmp", "webp", "gif", "avif"];

ef316d8f997cddb3e1a00894a6a9655b44953fc2
orw22 <oliver@thewortleys.plus.com>
2023-09-12 04:26:28 +0100

    readability

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
 pub enum Constants {
-    MaxFileSize = 2000000, // 2MB
+    MaxFileSize = 2_000_000, // 2MB
 }
 
-pub const IMAGE_EXTENSIONS: [&str; 7] = ["jpg", "jpeg", "png", "bmp", "webp", "gif", "avif"];
+pub const IMAGE_EXTENSIONS: [&str; 6] = ["jpg", "jpeg", "png", "bmp", "webp", "gif"];

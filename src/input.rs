cde98eb5e449320f72b3681cc86896ecbc0b3b2c
orw22 <oliver@thewortleys.plus.com>
2023-09-13 17:35:07 +0100

    Use dialoguer

--- src/input.rs
+++ src/input.rs
@@ -1,9 +1,6 @@
 use clap::Parser;
-use log::error;
-use std::{
-    io::{self, Write},
-    path::PathBuf,
-};
+use dialoguer::Confirm;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -16,25 +13,15 @@ pub fn process_args(path: Option<PathBuf>) -> Option<PathBuf> {
     match path {
         Some(_path) => return Some(_path),
         None => {
-            print!("!!! This will resize all the images in this directory to under 2MB. Are you sure you wish to continue? (y/n) ");
-            io::stdout().flush().unwrap();
-
-            let mut res = String::new();
-            io::stdin()
-                .read_line(&mut res)
-                .expect("Failed to read user input");
-
-            match res.trim().to_lowercase().as_str() {
-                "y" => {
-                    return Some(PathBuf::from("."));
-                }
-                "n" => {
-                    return None;
-                }
-                _ => {
-                    error!("Invalid input");
-                    return None;
-                }
+            if Confirm::new()
+                .with_prompt("! This will resize all the images in this directory to under 2MB. Are you sure you wish to continue?")
+                .wait_for_newline(true)
+                .interact()
+                .unwrap()
+            {
+                Some(PathBuf::from("."))
+            } else {
+                return None;
             }
         }
     }
